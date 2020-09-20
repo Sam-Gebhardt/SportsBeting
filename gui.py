52c00d129f161bdd9f928ac0128da8f813ccc0ed
Sam Gebhardt <samgeb35@gmail.com>
2020-09-20 13:08:15 -0700

    Turned buttons into class attributes

--- gui.py
+++ gui.py
@@ -8,10 +8,23 @@ def out():
     print("Testing")
 
 
+# def reset_data(data:dict):
+
+
 def confirm(data: dict):
     """Store given data in the db"""
     for i in data:
-        print(data.get(i).get())
+        data[i] = data[i].get()
+        print(data[i])
+
+    try:
+        int(data["Wager"])
+        int(data["Odds"])
+
+    except ValueError:
+        print("no")
+        # return False
+    # data.clear()
 
 
 def confirm_parley(data: dict):
@@ -32,6 +45,22 @@ class App(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        self.data = {}
+
+        self.label_sport = None
+        self.label_matchup = None
+        self.label_odds = None
+        self.label_wager = None
+        self.label_type = None
+        self.label_bet_on = None
+
+        self.sport_input = None
+        self.match_input = None
+        self.odds_input = None
+        self.wager_input = None
+        self._type_input = None
+        self.bet_on_input = None
+
         # self.logo()
         self.menu()
         self.start_page()
@@ -79,13 +108,12 @@ class App(tk.Frame):
 
     def open_bet_menu(self):
 
-        data = {}
-        label_sport = tk.Label(self.master, text="Sport")
-        label_type = tk.Label(self.master, text="Type")
-        label_matchup = tk.Label(self.master, text="Matchup")
-        label_bet_on = tk.Label(self.master, text="Bet")
-        label_odds = tk.Label(self.master, text="Odds")
-        label_wager = tk.Label(self.master, text="Wager")
+        self.label_sport = tk.Label(self.master, text="Sport")
+        self.label_type = tk.Label(self.master, text="Type")
+        self.label_matchup = tk.Label(self.master, text="Matchup")
+        self.label_bet_on = tk.Label(self.master, text="Bet")
+        self.label_odds = tk.Label(self.master, text="Odds")
+        self.label_wager = tk.Label(self.master, text="Wager")
 
         sport = tk.StringVar()
         _type = tk.StringVar()
@@ -94,77 +122,79 @@ class App(tk.Frame):
         odds = tk.StringVar()
         wager = tk.StringVar()
 
-        sport_input = tk.Entry(self.master, width=7, textvariable=sport)
-        _type_input = tk.Entry(self.master, width=7, textvariable=_type)
-        match_input = tk.Entry(self.master, width=20, textvariable=matchup)
-        bet_on_input = tk.Entry(self.master, width=20, textvariable=bet_on)
-        odds_input = tk.Entry(self.master, width=7, textvariable=odds)
-        wager_input = tk.Entry(self.master, width=7, textvariable=wager)
+        self.sport_input = tk.Entry(self.master, width=7, textvariable=sport)
+        self._type_input = tk.Entry(self.master, width=7, textvariable=_type)
+        self.match_input = tk.Entry(self.master, width=20, textvariable=matchup)
+        self.bet_on_input = tk.Entry(self.master, width=20, textvariable=bet_on)
+        self.odds_input = tk.Entry(self.master, width=7, textvariable=odds)
+        self.wager_input = tk.Entry(self.master, width=7, textvariable=wager)
 
-        data["Sport"] = sport
-        data["Type"] = _type
-        data["Matchup"] = matchup
-        data["Bet"] = bet_on
-        data["Odds"] = odds
-        data["Wager"] = wager
+        self.data["Sport"] = sport
+        self.data["Type"] = _type
+        self.data["Matchup"] = matchup
+        self.data["Bet"] = bet_on
+        self.data["Odds"] = odds
+        self.data["Wager"] = wager
 
-        sport_input.grid(row=1, column=2)
-        label_sport.grid(row=2, column=2)
+        self.sport_input.grid(row=1, column=2)
+        self.label_sport.grid(row=2, column=2)
 
-        _type_input.grid(row=1, column=3)
-        label_type.grid(row=2, column=3)
+        self._type_input.grid(row=1, column=3)
+        self.label_type.grid(row=2, column=3)
 
-        match_input.grid(row=1, column=4)
-        label_matchup.grid(row=2, column=4)
+        self.match_input.grid(row=1, column=4)
+        self.label_matchup.grid(row=2, column=4)
 
-        bet_on_input.grid(row=1, column=5, pady=30)
-        label_bet_on.grid(row=2, column=5)
+        self.bet_on_input.grid(row=1, column=5, pady=30)
+        self.label_bet_on.grid(row=2, column=5)
 
-        odds_input.grid(row=1, column=6)
-        label_odds.grid(row=2, column=6)
+        self.odds_input.grid(row=1, column=6)
+        self.label_odds.grid(row=2, column=6)
 
-        wager_input.grid(row=1, column=7)
-        label_wager.grid(row=2, column=7)
+        self.wager_input.grid(row=1, column=7)
+        self.label_wager.grid(row=2, column=7)
 
-        button = tk.Button(self.master, text="Confirm", command=lambda: confirm(data), bd='5', bg="green")
+        button = tk.Button(self.master, text="Confirm", command=lambda: confirm(self.data), bd='5', bg="green")
         button.grid(row=3, column=4, pady=3)
 
     def open_parley_menu(self):
-        data = {}
-        label_sport = tk.Label(self.master, text="Sport")
-        label_matchup = tk.Label(self.master, text="Matchup(s) separated by a ';'")
-        label_odds = tk.Label(self.master, text="Odds")
-        label_wager = tk.Label(self.master, text="Wager")
+        self.label_sport = tk.Label(self.master, text="Sport")
+        self.label_matchup = tk.Label(self.master, text="Matchup(s) separated by a ';'")
+        self.label_odds = tk.Label(self.master, text="Odds")
+        self.label_wager = tk.Label(self.master, text="Wager")
 
         sport = tk.StringVar()
         matchup = tk.StringVar()
         odds = tk.StringVar()
         wager = tk.StringVar()
 
-        sport_input = tk.Entry(self.master, width=7, textvariable=sport)
-        match_input = tk.Entry(self.master, width=48, textvariable=matchup)
-        odds_input = tk.Entry(self.master, width=7, textvariable=odds)
-        wager_input = tk.Entry(self.master, width=7, textvariable=wager)
+        self.sport_input = tk.Entry(self.master, width=7, textvariable=sport)
+        self.match_input = tk.Entry(self.master, width=48, textvariable=matchup)
+        self.odds_input = tk.Entry(self.master, width=7, textvariable=odds)
+        self.wager_input = tk.Entry(self.master, width=7, textvariable=wager)
 
-        data["Sport"] = sport
-        data["Matchup"] = matchup
-        data["Odds"] = odds
-        data["Wager"] = wager
+        self.data["Sport"] = sport
+        self.data["Matchup"] = matchup
+        self.data["Odds"] = odds
+        self.data["Wager"] = wager
 
-        sport_input.grid(row=1, column=2)
-        label_sport.grid(row=2, column=2)
+        self.sport_input.grid(row=1, column=2)
+        self.label_sport.grid(row=2, column=2)
 
-        match_input.grid(row=1, column=4)
-        label_matchup.grid(row=2, column=4)
+        self.match_input.grid(row=1, column=4)
+        self.label_matchup.grid(row=2, column=4)
 
-        odds_input.grid(row=1, column=6)
-        label_odds.grid(row=2, column=6)
+        self.odds_input.grid(row=1, column=6)
+        self.label_odds.grid(row=2, column=6)
 
-        wager_input.grid(row=1, column=7)
-        label_wager.grid(row=2, column=7)
+        self.wager_input.grid(row=1, column=7)
+        self.label_wager.grid(row=2, column=7)
 
-        button = tk.Button(self.master, text="Confirm", command=lambda: confirm_parley(data), bd='5', bg="green")
+        button = tk.Button(self.master, text="Confirm", command=lambda: confirm_parley(self.data), bd='5', bg="green")
         button.grid(row=3, column=4, pady=3)
+
+    def confirm(self):
+        """"""
 
 
 if __name__ == "__main__":
