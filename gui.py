8d1ea5c6b3cf50ab8970f7dc2bdbb3735b0eb9df
Sam Gebhardt <samgeb35@gmail.com>
2020-09-19 19:58:30 -0700

    Button improvements

--- gui.py
+++ gui.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 
+# todo: make functions for testing, but cut them out and
+#  alter functions in bet.py to take a dict as an arg
+
 
 def out():
     print("Testing")
@@ -11,6 +14,19 @@ def confirm(data: dict):
         print(data.get(i).get())
 
 
+def confirm_parley(data: dict):
+
+    for i in data:
+        if i == "Matchup":
+            separated = data.get(i).get().split(";")
+            for j in separated:
+                j = j.strip()
+                if j:
+                    print(j)
+        else:
+            print(data.get(i).get())
+
+
 class App(tk.Frame):
 
     def __init__(self, master=None):
@@ -19,18 +35,18 @@ class App(tk.Frame):
         # self.logo()
         self.menu()
         self.start_page()
-        self.input_box()
+        # self.input_box()
 
     def start_page(self):
-        btn = tk.Button(self.master, text='Exit', bd='5', command=self.master.destroy)
+        btn = tk.Button(self.master, text='Exit', bd='5', command=self.master.destroy, bg="red")
         btn.grid(row=3, column=2, pady=10)
 
     def menu(self):
         menu_bar = tk.Menu(self.master)
 
         _open = tk.Menu(menu_bar, tearoff=0)
-        _open.add_command(label="Bet", command=None)
-        _open.add_command(label="Parley")
+        _open.add_command(label="Bet", command=self.open_bet_menu)
+        _open.add_command(label="Parley", command=self.open_parley_menu)
         menu_bar.add_cascade(label="Open", menu=_open)
 
         _close = tk.Menu(menu_bar, tearoff=0)
@@ -61,7 +77,7 @@ class App(tk.Frame):
         label.grid()
         canvas.create_image(20, 20, anchor="nw", image=img)
 
-    def input_box(self):
+    def open_bet_menu(self):
 
         data = {}
         label_sport = tk.Label(self.master, text="Sport")
@@ -83,7 +99,7 @@ class App(tk.Frame):
         match_input = tk.Entry(self.master, width=20, textvariable=matchup)
         bet_on_input = tk.Entry(self.master, width=20, textvariable=bet_on)
         odds_input = tk.Entry(self.master, width=7, textvariable=odds)
-        wager_input = tk.Entry(self.master, width=5, textvariable=wager)
+        wager_input = tk.Entry(self.master, width=7, textvariable=wager)
 
         data["Sport"] = sport
         data["Type"] = _type
@@ -110,7 +126,44 @@ class App(tk.Frame):
         wager_input.grid(row=1, column=7)
         label_wager.grid(row=2, column=7)
 
-        button = tk.Button(self.master, text="Confirm", command=lambda: confirm(data))
+        button = tk.Button(self.master, text="Confirm", command=lambda: confirm(data), bd='5', bg="green")
+        button.grid(row=3, column=4, pady=3)
+
+    def open_parley_menu(self):
+        data = {}
+        label_sport = tk.Label(self.master, text="Sport")
+        label_matchup = tk.Label(self.master, text="Matchup(s) separated by a ';'")
+        label_odds = tk.Label(self.master, text="Odds")
+        label_wager = tk.Label(self.master, text="Wager")
+
+        sport = tk.StringVar()
+        matchup = tk.StringVar()
+        odds = tk.StringVar()
+        wager = tk.StringVar()
+
+        sport_input = tk.Entry(self.master, width=7, textvariable=sport)
+        match_input = tk.Entry(self.master, width=48, textvariable=matchup)
+        odds_input = tk.Entry(self.master, width=7, textvariable=odds)
+        wager_input = tk.Entry(self.master, width=7, textvariable=wager)
+
+        data["Sport"] = sport
+        data["Matchup"] = matchup
+        data["Odds"] = odds
+        data["Wager"] = wager
+
+        sport_input.grid(row=1, column=2)
+        label_sport.grid(row=2, column=2)
+
+        match_input.grid(row=1, column=4)
+        label_matchup.grid(row=2, column=4)
+
+        odds_input.grid(row=1, column=6)
+        label_odds.grid(row=2, column=6)
+
+        wager_input.grid(row=1, column=7)
+        label_wager.grid(row=2, column=7)
+
+        button = tk.Button(self.master, text="Confirm", command=lambda: confirm_parley(data), bd='5', bg="green")
         button.grid(row=3, column=4, pady=3)
 
 
